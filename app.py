40afac19852ce19721a9c96351c0e881324ca69a
Appy <appy.belsare@gmail.com>
2026-02-25 19:22:50 +0530

    added document upload functionality .pdf .docx

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.postprocessor import SentenceTransformerRerank
-
+from llama_index.core.retrievers import VectorIndexRetriever, QueryFusionRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.retrievers.bm25 import BM25Retriever
 # -----------------------------
 # Configure local models
 # -----------------------------
@@ -21,8 +23,11 @@ Settings.llm = Ollama(
 # Load documents
 # -----------------------------
 
-documents = SimpleDirectoryReader("documents").load_data()
 
+documents = SimpleDirectoryReader(
+    "documents",
+    required_exts=[".txt", ".pdf", ".docx"]
+).load_data()
 # -----------------------------
 # Better chunking
 # -----------------------------
@@ -45,23 +50,41 @@ index = VectorStoreIndex(nodes)
 # -----------------------------
 
 # reranker improves result ordering
+# vector retriever
+vector_retriever = VectorIndexRetriever(
+    index=index,
+    similarity_top_k=6,
+)
+
+# keyword retriever
+bm25_retriever = BM25Retriever.from_defaults(
+    nodes=nodes,
+    similarity_top_k=6,
+)
+
+# combine both
+fusion_retriever = QueryFusionRetriever(
+    [vector_retriever, bm25_retriever],
+    similarity_top_k=6,
+)
+
+# reranker (keep this!)
+from llama_index.core.postprocessor import SentenceTransformerRerank
+
 reranker = SentenceTransformerRerank(
     model="cross-encoder/ms-marco-MiniLM-L-6-v2",
-    top_n=2   # keep best 2 chunks
+    top_n=2
 )
 
-query_engine = index.as_query_engine(
-    similarity_top_k=6,   # retrieve more candidates
+query_engine = RetrieverQueryEngine.from_args(
+    fusion_retriever,
     node_postprocessors=[reranker]
 )
-
 # -----------------------------
 # Ask question
 # -----------------------------
 
-response = query_engine.query("What problem does RAG solve?" +
- "How does vector search work?"+
-"Explain RAG like I'm a beginner")
+response = query_engine.query("who is the headmaster?")
 
 print("\nAnswer:")
 print(response)
